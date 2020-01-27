3793ff02875bfde3ea85ab838a99466a1fa6ef72
Harish Sanjaykumar Pukale <46403164+harishsanjaykumarpukale@users.noreply.github.com>
2020-01-28 00:03:49 +0530

    Update rsa.h

--- rsa.h
+++ rsa.h
@@ -17,5 +17,73 @@ mpint newInt(){
 }
 
 
+void assignInt(mpint *n,char c[]){
+
+	int i,num,r=33554432,k=0,j=0,inpSize=strlen(c);
+	long int size=( inpSize / 8) + 1;
+	char str[9];
+	int que[inpSize];
+
+	//strcpy(que,c);
+
+	n->word = (long* ) calloc( 2 * size ,  sizeof(long));
+
+	for(i=0;i<8;i++)
+		str[i]=c[i];
+	num = atoi(str);
+
+	if(inpSize<8){
+
+		n->word[0]=num;
+
+                return ;
+        }
+
+	do {
+		for(;i<inpSize;i++){
+			que[k++]=num/r;
+			num=(num%r)*10 + c[i] -'0';
+			}
+
+		que[k++]=num/r;
+		que[k++]='\0';
+
+		n->word[j++]=num % r;
+
+		printf("\nbefore c:%s",c);
+
+		printf("\nbefore que:");
+
+		for(k=0;k<inpSize;k++)
+			printf("%d",que[k]);
+
+		//strcpy(c,que);
+
+		printf("\nafter c:%s",c);
+		printf("\nafter que:");
+
+		for(k=0;k<inpSize;k++)
+			printf("%d",que[k]);
+		
+		inpSize=sizeof(que); //or inpSize=sizeof(c);
+
+		for(i=0;i<inpSize;i++)
+			str[i]=c[i];
+		
+		num = atoi(str);
+
+		printf("\n%d\n",num);
+
+		k=0;
+	} while(num>=r);
+
+	n->word[j++]=num;
+	n->size=j;
+
+	n->sign='+'; //to be discussed how to set it 
+}
+
+
+
 
 #endif
